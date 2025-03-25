70d94273b9ca3d187ad6ec84892339c272e8df2d
UserLog <108153253+IvPCInc@users.noreply.github.com>
2025-03-25 16:55:59 +0300

    Update Tern1.js

--- Tern1.js
+++ Tern1.js
@@ -1,35 +1,63 @@
 let a = Math.floor(Math.random() * 100);
 
-
 // if...else
 let result;
+
 if ((a > 10 ? a : a * 2) > 5) {
-    if ((2 * a) + 1 > 4) {
-        result = 5;
-    } else {
-        result = (a % 2 == 0 ? 6 : 7);
-    }
+  result = (2 * a) + 1;
 } else {
-    result = (a < 3) ? 1 : 2 * (a - 2);
+  if ((a < 3 ? 1 : 2 * (a - 2)) > 4) {
+    result = 5;
+  } else {
+    result = (a % 2 === 0 ? 6 : 7);
+  }
 }
 
 console.log("Результат if...else: ", result);
 
 // switch
-switch(true) {
-    case (a > 10 ? a : a * 2) > 5:
-        switch(true) {
-            case (2 * a) + 1 > 4:
-                result = 5;
-                break;
-            default:
-                result = (a % 2 == 0 ? 6 : 7);
-                break;
-        }
-        break;
-    default:
-        result = (a < 3) ? 1 : 2 * (a - 2);
-        break;
+let resultSwitch;
+
+if ((a > 10 ? a : a * 2) > 5) {
+  resultSwitch = (2 * a) + 1;
+} else {
+  let intermediateResult = (a < 3 ? 1 : 2 * (a - 2));
+    if (intermediateResult > 4) {
+        resultSwitch = 5;
+    } else {
+        resultSwitch = (a % 2 === 0 ? 6 : 7);
+    }
 }
 
-console.log("Результат switch: ", result);
+console.log("Результат switch: ", resultSwitch);let a = Math.floor(Math.random() * 100);
+
+// if...else
+let result;
+
+if ((a > 10 ? a : a * 2) > 5) {
+  result = (2 * a) + 1;
+} else {
+  if ((a < 3 ? 1 : 2 * (a - 2)) > 4) {
+    result = 5;
+  } else {
+    result = (a % 2 === 0 ? 6 : 7);
+  }
+}
+
+console.log("Результат if...else: ", result);
+
+// switch
+let resultSwitch;
+
+if ((a > 10 ? a : a * 2) > 5) {
+  resultSwitch = (2 * a) + 1;
+} else {
+  let intermediateResult = (a < 3 ? 1 : 2 * (a - 2));
+    if (intermediateResult > 4) {
+        resultSwitch = 5;
+    } else {
+        resultSwitch = (a % 2 === 0 ? 6 : 7);
+    }
+}
+
+console.log("Результат switch: ", resultSwitch);
